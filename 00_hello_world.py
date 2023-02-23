bddde3843f265212b3c4eaae71f47fb7577a6589
juansensio <sensio.juan@gmail.com>
2023-02-23 13:21:54 +0100

    update

--- 00_hello_world.py
+++ 00_hello_world.py
@@ -4,13 +4,13 @@ import os
 class YourComponent(L.LightningWork):
    def run(self):
       print('RUN ANY PYTHON CODE HERE')
-      # print(os.listdir('/foo'))
+      print(os.listdir('/foo'))
 
 # basic component
 # component = YourComponent()
 
 # run on a cloud machine ("cpu", "gpu", ...)
-# compute = L.CloudCompute("gpu")
+compute = L.CloudCompute("gpu")
 
 # stop the machine when idle for 10 seconds
 # compute = L.CloudCompute('gpu', idle_timeout=10)
@@ -26,8 +26,8 @@ class YourComponent(L.LightningWork):
 # compute = L.CloudCompute('gpu', disk_size=100)
 
 # mount the files on the s3 bucket under this path
-mount = L.storage.Mount(source="s3://lightning-example-public/", mount_path="/foo") # no va
-compute = L.CloudCompute(mounts=mount)
+# mount = L.storage.Mount(source="s3://lightning-example-public/", mount_path="/foo") # no va
+# compute = L.CloudCompute(mounts=mount)
 
 component = YourComponent(cloud_compute=compute)
 app = L.LightningApp(component)
