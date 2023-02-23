d6e4a921045a7a46bf5cb07a934ed957d2791d09
juansensio <sensio.juan@gmail.com>
2023-02-23 13:18:50 +0100

    update

--- 00_hello_world.py
+++ 00_hello_world.py
@@ -13,7 +13,7 @@ class YourComponent(L.LightningWork):
 # compute = L.CloudCompute("gpu")
 
 # stop the machine when idle for 10 seconds
-compute = L.CloudCompute('gpu', idle_timeout=10)
+# compute = L.CloudCompute('gpu', idle_timeout=10)
 
 # if the machine hasn't started after 60 seconds, cancel the work
 # compute = L.CloudCompute('gpu', wait_timeout=60)
@@ -26,8 +26,8 @@ compute = L.CloudCompute('gpu', idle_timeout=10)
 # compute = L.CloudCompute('gpu', disk_size=100)
 
 # mount the files on the s3 bucket under this path
-# mount = L.Mount(source="s3://lightning-example-public/", mount_path="/foo") # no va
-# compute = L.CloudCompute(mounts=mount)
+mount = L.storage.Mount(source="s3://lightning-example-public/", mount_path="/foo") # no va
+compute = L.CloudCompute(mounts=mount)
 
 component = YourComponent(cloud_compute=compute)
 app = L.LightningApp(component)
